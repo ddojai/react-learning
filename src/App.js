7a4af65d3872fa3e0b31d4c7b785a5004df2cd6a
김흥열 <favianred@gmail.com>
2018-10-29 16:48:08 +0900

    * 누구든지 하는 리액트 (https://react-anyone.vlpt.us)   * ch9     * 데이터 필터링, 불변성, 최적화

--- src/App.js
+++ src/App.js
@@ -16,7 +16,14 @@ class App extends Component {
         name: '홍길동',
         phone: '010-0000-0001'
       }
-    ]
+    ],
+    keyword: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    })
   };
 
   handleCreate = (data) => {
@@ -38,20 +45,32 @@ class App extends Component {
     this.setState({
       information: information.map(
         info => id === info.id
-        ? {...info, ...data} // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
-        : info // 기존의 값을 그대로 유지
+          ? {...info, ...data} // 새 객체를 만들어서 기존의 값과 전달받은 data 을 덮어씀
+          : info // 기존의 값을 그대로 유지
       )
     })
   };
 
   render() {
+    const {information, keyword} = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
+
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        <p>
+          <input
+            placeholder="검색 할 이름을 입력하세요.."
+            onChange={this.handleChange}
+            value={keyword}
+          />
+        </p>
         <PhoneInfoList
-          data={this.state.information}
+          data={filteredList}
           onRemove={this.handleRemove}
           onUpdate={this.handleUpdate}
         />
