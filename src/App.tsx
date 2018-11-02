fb0d813d185d72162fff65c39d43bf9133b2db8a
김흥열 <favianred@gmail.com>
2018-11-02 15:04:52 +0900

    * TypeScript with React + Redux/Immutable.js 빠르게 배우기 (https://velopert.com/3595)   1. TypeScript & React 프로젝트 시작하기   2. 카운터 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
+import Counter from "./components/Counter";
+import Profile from "./components/Profile";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <Profile
+          name="벨로퍼트"
+          job="코드사랑꾼"
+        />
+        <Counter counter={0}/>
       </div>
     );
   }
