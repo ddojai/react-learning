67043fe8e96bcd6b005528a767ac3fff45c77da2
김흥열 <favianred@gmail.com>
2018-11-08 18:31:50 +0900

    * 리액트를 다루는 기술 (10장 일정관리앱)   * 10.3 상태관리     * 데이터 입력

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,56 @@ import TodoInput from "./TodoInput/TodoInput";
 import TodoList from "./TodoList/TodoList";
 
 class App extends Component {
+  state = {
+    input: '', // input 값
+    // 일정 데이터 초깃값
+    todos: [
+      {id: 0, text: '리액트 공부하기', done: true},
+      {id: 1, text: '컴포넌트 스타일링 해보기', done: false},
+    ]
+  };
+
+  // 일정 데이터 안에 들어가는 id 값
+  id = 1;
+  getId = () => {
+    return ++this.id;
+  };
+
+  handleChange = (e) => {
+    const {value} = e.target;
+    this.setState({
+      input: value
+    })
+  };
+
+  handleInsert = () => {
+    const {todos, input} = this.state;
+
+    // 새 데이터 객체 만들기
+    const newTodo = {
+      text: input,
+      done: false,
+      id: this.getId()
+    };
+
+    // 배열 안에 새 데이터를 집어 넣습니다.
+    this.setState({
+      todos:[...todos, newTodo],
+      input:''
+    })
+  };
+
   render() {
+    const {input, todos} = this.state;
+    const {
+      handleChange,
+      handleInsert
+    } = this;
+
     return (
       <PageTemplate>
-        <TodoInput/>
-        <TodoList/>
+        <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
+        <TodoList todos={todos}/>
       </PageTemplate>
     );
   }
