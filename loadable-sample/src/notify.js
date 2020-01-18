4cc0a909829c91e6de8e850e10dbe8063e1930a3
ddojai <favianred@gmail.com>
2020-01-18 17:14:05 +0900

    * Remove legacy project

--- loadable-sample/src/notify.js
+++
@@ -1,5 +0,0 @@
-function notify() {
-  alert('종소리 울려라 종소리 울려~');
-}
-
-export default notify;
--- nasa-apod/src/lib/api.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export function getAPOD(date = '') {
-  return axios.get(`https://api.nasa.gov/planetary/apod?api_key=j93ZsmvnjIA9rA4OnbMN52CHmBRS5TcK4wffyOsn&date=${date}`);
-}
