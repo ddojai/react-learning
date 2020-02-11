4b3af9284e41391a94d44799457157cf1ad85c76
ddojai <favianred@gmail.com>
2020-02-11 23:51:20 +0900

    * 리액트를 다루는 기술 - 개정판   * 23장 JWT를 통한 회원 인증 시스템 구현하기     * 23.6 username/tags로 포스트 필터링하기

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -72,7 +72,7 @@ export const write = async ctx => {
 };
 
 /*
-GET /api/posts
+GET /api/posts?username=&tag=&page=
  */
 export const list = async ctx => {
   // query는 문자열이기 때문에 숫자로 변환
@@ -84,17 +84,23 @@ export const list = async ctx => {
     return;
   }
 
+  const { tag, username } = ctx.query;
+  // tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts
-      .map(post => ({
+    ctx.body = posts.map(post => ({
         ...post,
         body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
       }));
