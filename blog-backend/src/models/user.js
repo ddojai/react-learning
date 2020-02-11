d4a479f0d452987b07022699ec66ca6658c71983
ddojai <favianred@gmail.com>
2020-02-11 22:14:38 +0900

    * 리액트를 다루는 기술 - 개정판   * 23장 JWT를 통한 회원 인증 시스템 구현하기     * 23.4 토큰 발급 및 검증하기

--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
   username: String,
@@ -18,6 +19,20 @@ UserSchema.methods.serialize = function() {
   const data = this.toJSON();
   delete data.hashedPassword;
   return data;
+};
+
+UserSchema.methods.generateToken = function () {
+  return jwt.sign(
+    // 첫 번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣습니다.
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣습니다.
+    {
+      expiresIn: '7d', // 7일 동안 유효함
+    },
+  );
 };
 
 UserSchema.statics.findByUsername = function (username) {
