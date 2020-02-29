3c5b986f4ee95d3007252ac8ba1109737916ab4a
ddojai <favianred@gmail.com>
2020-02-29 21:44:44 +0900

    * 26.2.3 HTML 필터링하기

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,8 +1,33 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
+import sanitizeHtml from 'sanitize-html';
 
 const { ObjectId } = mongoose.Types;
+
+const sanitizeOption = {
+  allowedTags: [
+    'h1',
+    'h2',
+    'b',
+    'i',
+    'u',
+    's',
+    'p',
+    'ul',
+    'ol',
+    'li',
+    'blockquote',
+    'a',
+    'img',
+  ],
+  allowedAttributes: {
+    a: ['href', 'name', 'target'],
+    img: ['src'],
+    li: ['class'],
+  },
+  allowedSchemes: ['data', 'http'],
+};
 
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
@@ -61,7 +86,10 @@ export const write = async ctx => {
 
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
-    title, body, tags, user: ctx.state.user,
+    title,
+    body: sanitizeHtml(body, sanitizeOption),
+    tags,
+    user: ctx.state.user,
   });
   try {
     await post.save();
@@ -69,6 +97,14 @@ export const write = async ctx => {
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+// html을 없애고 내용이 너무 길면 200자로 제한하는 함수
+const removeHtmlAndShorten = body => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  });
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`;
 };
 
 /*
@@ -101,9 +137,9 @@ export const list = async ctx => {
     const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map(post => ({
-        ...post,
-        body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
-      }));
+      ...post,
+      body: removeHtmlAndShorten(post.body),
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -155,8 +191,13 @@ export const update = async ctx => {
     return;
   }
 
+  const nextData = {...ctx.request.body}; // 객체를 복사하고
+  // body 값이 주어졌으면 HTML 필터링
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body);
+  }
   try {
-    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환
       // false 일 때는 업데이트 되기 전의 데이터를 반환합니다.
     }).exec();
