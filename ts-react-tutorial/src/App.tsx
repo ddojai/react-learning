1a0edc7a202d26192cfb31b798441d7f66266126
ddojai <favianred@gmail.com>
2020-05-17 21:07:59 +0900

    React with typescript4
    - https://velog.io/@velopert/using-hooks-with-typescript
    - useReducer

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import MyFrom from './MyFrom';
+import ReducerSample from './ReducerSample';
 
 const App: React.FC = () => {
-  const onSubmit = (form: { name: string; description: string}) => {
-    console.log(form);
-  };
-  return <MyFrom onSubmit={onSubmit} />;
+  return <ReducerSample/>
 };
 
 export default App;
